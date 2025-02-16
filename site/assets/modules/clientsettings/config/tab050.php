11b81ba994a09e4bc309c5ff0b5edada7a0885b6
ProjectSoft <projectsoft2009@yandex.ru>
2025-02-16 14:18:52 +0400

    update

--- site/assets/modules/clientsettings/config/tab050.php
+++ site/assets/modules/clientsettings/config/tab050.php
@@ -1,8 +1,32 @@
 <?php
+if( ! defined('IN_MANAGER_MODE') || IN_MANAGER_MODE !== true) {
+	die('<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please use the EVO Content Manager instead of accessing this file directly.');
+}
+
+global $modx;
+
+$str = "";
+$arrResult = array();
+
+$table_site_content = $modx->getFullTableName('site_content');
+$result = $modx->db->query( "SELECT id,pagetitle FROM $table_site_content WHERE parent = 0 AND published = 1 ORDER BY id ASC");
+
+while( $row = $modx->db->getRow( $result ) ) {
+	$arrResult[] = $row["pagetitle"] . " (" . $row["id"] . ")==" . $row["id"];
+}
+$str = implode('||', $arrResult);
+
 return array (
 	'caption' => 'НОВОСТИ',
 	'introtext' => '<b style="color: red">Настройки для НОВОСТЕЙ</b>',
 	'settings' => array (
+		'news_id' => array(
+			'caption' => 'Ресурс новостей',
+			'type' => 'dropdown',
+			'elements' => $str,
+			'note' => '',
+			'default_text' => '',
+		),
 		'news_count_home' => array(
 			'caption' => 'Количество новостей на главной странице',
 			'type' => 'number',
